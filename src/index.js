82cfe5b198f7c05fee9ff64aea292c35501cbee7
david-shakaya <devidoz666@gmail.com>
2021-07-28 00:16:00 +0300

    app done

--- src/index.js
+++ src/index.js
@@ -3,29 +3,43 @@ import './styles.scss';
 import worlds from './data'
 
 
-
 function returnedRandomIndex() {
     const lengthArrow = worlds.length
     const randomIndex = Math.floor(Math.random() * lengthArrow);
     return randomIndex
 }
 
-const randomIndex =  returnedRandomIndex()
 
 
-function returdUkranianWorld() {
-    const ua = worlds[randomIndex].uk
-    return ua
+// DOM- elements
+
+const btnNextWord = document.querySelector('.next-word');
+const btnShowAnswer = document.querySelector('.show-answer');
+const screen = document.querySelector('.screen');
+const screen2 = document.querySelector('.screen2');
+
+
+btnNextWord.addEventListener('click', nextWord)
+btnShowAnswer.addEventListener('click', showAnswer)
+
+
+let curentId;
+function nextWord() {
+    let rl = returnedRandomIndex()
+    const id = worlds[rl].id
+
+   const curentElem = worlds.find(el => {
+      return el.id === id
+    })
+
+    curentId = curentElem
+    screen.innerHTML = `<p>${worlds[rl].en}</p>`
+    btnShowAnswer.disabled = false;
+    
+    }
+
+function showAnswer() {
+    screen2.innerHTML = `<p>${curentId.uk}</p>`
+    btnShowAnswer.setAttribute("disabled", "disabled");
+    
 }
-
-
-function returdEnglishWorld() {
-    const en = worlds[randomIndex].en
-    return en
-}
-
-
-console.log(returdUkranianWorld());
-console.log(returdEnglishWorld());
-
-
