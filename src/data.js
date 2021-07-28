27645bea8b2644e96c2c81ccab97aa457a963858
Давид Шакая <66774063+david-shakaya@users.noreply.github.com>
2021-07-28 17:16:05 +0300

    a

--- src/data.js
+++ src/data.js
@@ -17,7 +17,75 @@ const worlds = [
     uk: 'Я был на улице когда это случилось',
     en: 'I was in outsaid when it happened',
   },
-  { id: 16, uk: 'Количество', en: 'amount' },
+  { id: 16, uk: 'Скорее всего, да', en: 'probably to yes' },
+  //
+  {
+    id: 17,
+    uk: 'Я согласен с твоими условиями',
+    en: 'I agree to your conditions',
+  },
+  { id: 18, uk: 'Превращается в', en: 'turn into' },
+  { id: 19, uk: 'Привычка', en: 'habits' },
+  { id: 20, uk: 'Зависимость', en: 'addiction' },
+  {
+    id: 21,
+    uk: 'У меня зависимость от курения',
+    en: 'I have a smoking addiction',
+  },
+  {
+    id: 22,
+    uk: 'Я был в замешательстве от иностранных знаков',
+    en: 'I was confused by foreign signs',
+  },
+  {
+    id: 23,
+    uk: 'Ну ты знаешь, я думаю как-то так',
+    en: 'Well you now, i mean something like this',
+  },
+
+  { id: 24, uk: 'С тех пор', en: 'since then' },
+  {
+    id: 25,
+    uk: 'Я действительно давно тебя не видел',
+    en: 'I really haven"t seen you in a long time',
+  },
+  {
+    id: 26,
+    uk: 'Человек может быть тем кем захочет',
+    en: 'A person can be whoever he wants',
+  },
+  { id: 27, uk: 'Когда это было?', en: 'When it was?' },
+  {
+    id: 28,
+    uk: 'Ты хотел бы сменить работу?',
+    en: 'Would you like to change jobs?',
+  },
+
+  {
+    id: 29,
+    uk: 'Я бы хотел получить новую работу',
+    en: 'I would like to get a new job',
+  },
+  {
+    id: 30,
+    uk: 'Он хотел бы лететь в чистом небе с птицами.',
+    en: 'He would like to fly in a clear sky with birds.',
+  },
+  {
+    id: 31,
+    uk: 'Я испортил воздух. Но об этом никто не узнал. Это сошло с рук.',
+    en: 'I messed up the air. But no one found out about it. It got away with it.',
+  },
+  { id: 32, uk: 'Не в том смылсе', en: 'In the wrong sense' },
+  {
+    id: 33,
+    uk: 'Работник который принял приглашение',
+    en: 'The employee who accepted the invitation',
+  },
+  { id: 34, uk: 'Общий', en: 'mutual (мйючл)' },
+  { id: 35, uk: 'Взаимопонимание', en: 'mutual understanding' },
+  { id: 36, uk: 'Общий друг', en: 'mutual friend' },
+  //
 ];
 
 export default worlds;
