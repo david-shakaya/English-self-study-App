4c5f2da9fbf5074a4976958340f5464228f3f848
Давид Шакая <66774063+david-shakaya@users.noreply.github.com>
2021-07-28 12:38:32 +0300

    df

--- src/data.js
+++ src/data.js
@@ -1,7 +1,23 @@
 const worlds = [
-    { id: 1, uk: 'Левый, влево', en: 'left' },
-    { id: 2, uk: 'правй', en: 'right' },
-    { id: 3, uk: 'Вперед', en: 'forward' }
-]
+  { id: 1, uk: 'Раздеватся', en: 'undress' },
+  { id: 2, uk: 'Примерять', en: 'try on' },
+  { id: 4, uk: 'Одевать', en: 'put on' },
+  { id: 5, uk: 'Вернуть обратно', en: 'take back' },
+  { id: 6, uk: 'По этому', en: 'that"s why' },
+  { id: 7, uk: 'Редко, не часто', en: 'seldom' },
+  { id: 8, uk: 'Вылетело из головы', en: 'it slipped my mind' },
+  { id: 9, uk: 'Предполагать, угадать', en: 'guess' },
+  { id: 10, uk: 'Разные', en: 'different' },
+  { id: 11, uk: 'Разделять, отделятся', en: 'separate' },
+  { id: 12, uk: 'Включая, в том числе', en: 'including' },
+  { id: 13, uk: 'Наверное', en: 'probably' },
+  { id: 14, uk: 'Типа того', en: 'sort of' },
+  {
+    id: 15,
+    uk: 'Я был на улице когда это случилось',
+    en: 'I was in outsaid when it happened',
+  },
+  { id: 16, uk: 'Количество', en: 'amount' },
+];
 
-export default worlds
+export default worlds;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import './styles.scss';
 
-import worlds from './data'
-
+import worlds from './data';
 
 function returnedRandomIndex() {
-    const lengthArrow = worlds.length
-    const randomIndex = Math.floor(Math.random() * lengthArrow);
-    return randomIndex
+  const lengthArrow = worlds.length;
+  const randomIndex = Math.floor(Math.random() * lengthArrow);
+  return randomIndex;
 }
-
-
 
 // DOM- elements
 
@@ -18,29 +15,25 @@ const btnShowAnswer = document.querySelector('.show-answer');
 const screen = document.querySelector('.screen');
 const screen2 = document.querySelector('.screen2');
 
-
-btnNextWord.addEventListener('click', nextWord)
-btnShowAnswer.addEventListener('click', showAnswer)
-
+btnNextWord.addEventListener('click', nextWord);
+btnShowAnswer.addEventListener('click', showAnswer);
 
 let curentId;
 function nextWord() {
-    let rl = returnedRandomIndex()
-    const id = worlds[rl].id
+  let rl = returnedRandomIndex();
+  const id = worlds[rl].id;
 
-   const curentElem = worlds.find(el => {
-      return el.id === id
-    })
+  const curentElem = worlds.find(el => {
+    return el.id === id;
+  });
 
-    curentId = curentElem
-    screen2.innerHTML = ''
-    screen.innerHTML = `<h2>${worlds[rl].en}</h2>`
-    btnShowAnswer.disabled = false;
-    
-    }
+  curentId = curentElem;
+  screen2.innerHTML = '';
+  screen.innerHTML = `<p class="screen-next-word">${worlds[rl].uk}</p>`;
+  btnShowAnswer.disabled = false;
+}
 
 function showAnswer() {
-    screen2.innerHTML = `<h2>${curentId.uk}</h2>`
-    btnShowAnswer.setAttribute("disabled", "disabled");
-    
+  screen2.innerHTML = `<p class="screen-answer">${curentId.en}</p>`;
+  btnShowAnswer.setAttribute('disabled', 'disabled');
 }
